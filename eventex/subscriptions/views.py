91942f909116b7c96825608789aa6d089ff35807
Rafael Borges <rafael.borges@luizalabs.com>
2020-08-08 11:43:06 -0300

    handles event subscription

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,8 +1,23 @@
+from django.core import mail
 from django.shortcuts import render
+from django.template.loader import render_to_string
+from django.http import HttpResponse, HttpResponseRedirect
+
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
-    context = {'form': SubscriptionForm()}
+    if request.method == 'POST':
+        form = SubscriptionForm(request.POST)
+        form.full_clean()
+        body = render_to_string(
+            'subscriptions/subscription_email.txt', form.cleaned_data)
+        mail.send_mail('Confirmação de inscrição',
+                       body,
+                       'contato@eventex.com.br',
+                       ['recipient@gmail.com', form.cleaned_data['email']])
 
+        return HttpResponseRedirect('/inscricao/')
+
+    context = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
