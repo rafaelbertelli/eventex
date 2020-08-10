20d405dfc321a147ee5d5d51d34c82367dcc026c
Rafael Borges <rafael.borges@luizalabs.com>
2020-08-10 19:02:34 -0300

    refactor subscribe method

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,30 +1,44 @@
 from django.core import mail
+from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render
 from django.template.loader import render_to_string
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 
 from eventex.subscriptions.forms import SubscriptionForm
 
 
 def subscribe(request):
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
+        return create(request)
+    else:
+        return new(request)
 
-        if not form.is_valid():
-            context = {'form': form}
-            return render(request, 'subscriptions/subscription_form.html', context)
 
-        form.full_clean()
-        body = render_to_string(
-            'subscriptions/subscription_email.txt', form.cleaned_data)
-
-        mail.send_mail('Confirmação de inscrição', body,
-                       'contato@eventex.com.br', [form.cleaned_data['email']])
-
-        messages.success(request, 'Inscrição realizada com sucesso!')
-
-        return HttpResponseRedirect('/inscricao/')
-
+def new(request):
     context = {'form': SubscriptionForm()}
     return render(request, 'subscriptions/subscription_form.html', context)
+
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+
+    if not form.is_valid():
+        context = {'form': form}
+        return render(request, 'subscriptions/subscription_form.html', context)
+
+    template_name = 'subscriptions/subscription_email.txt'
+    subject = 'Confirmação de inscrição'
+    sender = settings.DEFAULT_FROM_EMAIL
+    context = form.cleaned_data
+    recipient = [context['email']]
+
+    _send_email(template_name, subject, sender, context, recipient)
+    messages.success(request, 'Inscrição realizada com sucesso!')
+
+    return HttpResponseRedirect('/inscricao/')
+
+
+def _send_email(template_name, subject, sender, context, recipient):
+    body = render_to_string(template_name, context)
+    mail.send_mail(subject, body, sender, recipient)
