11d3c119537b222982a7b141be9bfcc8d8808c05
Rafael Borges <rafael.borges@luizalabs.com>
2021-03-04 11:54:40 -0300

    admin site

--- eventex/settings.py
+++ eventex/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'test_without_migrations',
     'django_extensions',
     'eventex.core',
-    'eventex.subscriptions',
+    'eventex.subscriptions.apps.SubscriptionsConfig',
 ]
 
 MIDDLEWARE = [
