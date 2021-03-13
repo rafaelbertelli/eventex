b4bf20f71cd8f2b3b40fd60d347e3042aafb4c04
Rafael Borges <rafael.borges@luizalabs.com>
2021-03-13 18:11:05 -0300

    capitalize name

--- eventex/subscriptions/tests/test_form_subscribe.py
+++ eventex/subscriptions/tests/test_form_subscribe.py
@@ -3,6 +3,20 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscribeFormTest(TestCase):
+    def make_validated_form(self, **kwargs):
+        valid_data = dict(
+            name="Rafael", cpf='12345678912', email='rafael@protonmail.com', phone='1144221133')
+        data = dict(valid_data, **kwargs)
+        form = SubscriptionForm(data)
+        form.is_valid()
+        return form
+
+    def assert_form_error_code(self, form, field, code):
+        errors = form.errors.as_data()
+        errors_list = errors[field]
+        exception = errors_list[0]
+        self.assertEqual(code, exception.code)
+
     def test_form_has_fields(self):
         """Form must have 4 fields"""
         form = SubscriptionForm()
@@ -12,22 +26,12 @@ class SubscribeFormTest(TestCase):
     def test_cpf_is_digit(self):
         """CPF only accept digits"""
         form = self.make_validated_form(cpf='ABCD12312312')
-        self.assertFormErrorCode(form, 'cpf', 'digits')
+        self.assert_form_error_code(form, 'cpf', 'digits')
 
     def test_cpf_has_11_digits(self):
         form = self.make_validated_form(cpf='1212')
-        self.assertFormErrorCode(form, 'cpf', 'length')
+        self.assert_form_error_code(form, 'cpf', 'length')
 
-    def make_validated_form(self, **kwargs):
-        valid_data = dict(
-            name="Rafael", cpf='12345678912', email='rafael@protonmail.com', phone='1144221133')
-        data = dict(valid_data, **kwargs)
-        form = SubscriptionForm(data)
-        form.is_valid()
-        return form
-
-    def assertFormErrorCode(self, form, field, code):
-        errors = form.errors.as_data()
-        errors_list = errors[field]
-        exception = errors_list[0]
-        self.assertEqual(code, exception.code)
+    def test_name_must_be_captalized(self):
+        form = self.make_validated_form(name='MIGUEL AugusTO')
+        self.assertEqual('Miguel Augusto', form.cleaned_data['name'])
