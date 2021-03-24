01d70da58e9195ff69ea3bffaa4c987b0a9d3d4d
Rafael Borges <rafael.borges@luizalabs.com>
2021-03-24 14:24:06 -0300

    add speakers section

--- eventex/core/views.py
+++ eventex/core/views.py
@@ -2,4 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'index.html')
+    speakers = [
+        {
+            "name": "Grace Hopper",
+            "photo": 'http://hbn.link/hopper-pic'
+        },
+        {
+            "name": "Alan Turing",
+            "photo": 'http://hbn.link/turing-pic'
+        },
+    ]
+
+    context = { 'speakers': speakers }
+
+    return render(request, 'index.html', context)
